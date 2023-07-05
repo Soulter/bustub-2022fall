6e1a855f077be57e47cc58717ffd18e5c4d33a21
Soulter <37870767+Soulter@users.noreply.github.com>
2023-07-05 08:47:06 +0000

    feat: lru-k, extendible-hash

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -16,14 +16,168 @@ namespace bustub {
 
 LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+    printf("====\nhistory_list: ");
+    for(auto it=history_list.begin(); it!=history_list.end(); it++) {
+        printf("%d ", it->first);
+    }
+    printf("\naccess_list: ");
+    for(auto it=access_list.begin(); it!=access_list.end(); it++) {
+        printf("%d ", *it);
+    }
+    printf("\n====\n");
+    // std::scoped_lock<std::mutex> lock(latch_);
+    if(curr_size_ == 0 || history_list.empty()) {
+        printf("Evict fail\n");
+        return false;
+    }
+    
+    // frame_id_t minI = history_list.end()->first;
+    
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id) {}
+    for(auto it=history_list.begin(); it!=history_list.end(); it++) {
+        if(evictable_map[it->first]) {
+            //删除
+            evictable_map.erase(it->first);
+            curr_size_--;
+            *frame_id = it->first;
+            history_list.erase(it);
+            printf("Evict success frame_id: %d\n", *frame_id);
+            return true;
+        }
+    }
+    printf("Evict fail");
+    return false;
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+}
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+    BUSTUB_ASSERT(frame_id<=(int)replacer_size_, "frame_id is out of range");
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+    printf("====\nhistory_list: ");
+    for(auto it=history_list.begin(); it!=history_list.end(); it++) {
+        printf("%d ", it->first);
+    }
+    printf("\naccess_list: ");
+    for(auto it=access_list.begin(); it!=access_list.end(); it++) {
+        printf("%d ", *it);
+    }
+    printf("\n====\n");
+
+    if(evictable_map.find(frame_id) == evictable_map.end()) {
+        printf("new in frame_id: %d\n", frame_id);
+        // 新加入的frame。
+        if(curr_size_ == replacer_size_) {
+            printf("RecordAccess full. evict\n");
+            Evict(&frame_id);
+        }
+        SetEvictable(frame_id, true);
+
+        if(history_list.size() != 0) {
+            for(auto it2=history_list.rbegin(); it2!=history_list.rend(); it2++) {
+                //如果遇到==k的,就跳过
+                if(it2->second == k_) {
+                    continue;
+                } else {
+                    //插入到it2的后面
+                    history_list.insert(it2.base(), std::make_pair(frame_id, 1));
+                    // history_pos_rec[frame_id] = history_list.end();
+                    break;
+                }
+            }
+        } else {
+            history_list.push_back(std::make_pair(frame_id, 1));
+            // history_pos_rec[frame_id] = history_list.end();
+        }
+
+        
+
+    } else {
+        printf("not new in frame_id: %d\n", frame_id);
+        // frame_id_t pos = history_pos_rec[frame_id];
+        // 查询history_list中对应的是否达到k, 如果达到k, 则将其evictable_map中的值设为false
+        for(auto it=history_list.begin(); it!=history_list.end(); it++) {
+            if(it->first == frame_id) {
+                // if(it->second == k_) {
+                //     history_list.erase(it); 
+                //     if(access_list.size() == k_) {
+                //         access_list.pop_front();
+                //     }
+                //     access_list.push_back(frame_id);
+                //     SetEvictable(frame_id, false);
+                // } else {
+
+
+                // }
+                for(auto it2=history_list.rbegin(); it2!=history_list.rend(); it2++) {
+                    //如果遇到==k的,就跳过
+                    if(it2->second == k_ && it->second+1 < k_) {
+                        continue;
+                    } else {
+                        history_list.insert(it2.base(), std::make_pair(frame_id, it->second + 1));
+                        printf("|-> frame_id: %d, it->second+1: %ld\n", frame_id, it->second+1);
+                        history_list.erase(it);
+                        // history_pos_rec[frame_id] = history_list.end();
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
+
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+    BUSTUB_ASSERT(frame_id<=(int)replacer_size_, "frame_id is out of range");
+
+    printf("====\nhistory_list: ");
+    for(auto it=history_list.begin(); it!=history_list.end(); it++) {
+        printf("%d ", it->first);
+    }
+    printf("\naccess_list: ");
+    for(auto it=access_list.begin(); it!=access_list.end(); it++) {
+        printf("%d ", *it);
+    }
+    printf("\n====\n");
+
+    if(!evictable_map[frame_id] && set_evictable) {
+        evictable_map[frame_id] = true;
+        curr_size_++;
+
+        printf("SetEvictable frame_id: %d. now curr_size_: %ld\n", frame_id, curr_size_);
+    } else if (evictable_map[frame_id] && !set_evictable) {
+
+        evictable_map[frame_id] = false;
+        curr_size_--;
+        printf("SetUnEvictable frame_id: %d. now curr_size_: %ld\n", frame_id, curr_size_);
+    }
+}
+
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+    // std::scoped_lock<std::mutex> lock(latch_);
+    BUSTUB_ASSERT(frame_id<=(int)replacer_size_, "frame_id is out of range");
+    BUSTUB_ASSERT(!evictable_map[frame_id], "frame_id is not evictable");
+    if(evictable_map.find(frame_id) == evictable_map.end()) {
+        return;
+    }
+    if(history_list.size() == 0) {
+        return;
+    }
+    evictable_map.erase(frame_id);
+    std::list<std::pair<frame_id_t, size_t>>::iterator it = history_list.begin();
+    for(; it!=history_list.end(); it++) {
+        if(it->first == frame_id) {
+            history_list.erase(it);
+            break;
+        }
+    }
+    curr_size_--;
+}
+
+auto LRUKReplacer::Size() -> size_t {
+
+    std::scoped_lock<std::mutex> lock(latch_);
+    return curr_size_;
+}
 
 }  // namespace bustub
--- src/container/hash/extendible_hash_table.cpp
+++ src/container/hash/extendible_hash_table.cpp
@@ -23,10 +23,14 @@ namespace bustub {
 
 template <typename K, typename V>
 ExtendibleHashTable<K, V>::ExtendibleHashTable(size_t bucket_size)
-    : global_depth_(0), bucket_size_(bucket_size), num_buckets_(1) {}
+    : global_depth_(0), bucket_size_(bucket_size), num_buckets_(1) {
+  auto b = std::make_shared<Bucket>(bucket_size, 0);
+  dir_.push_back(b);
+}
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::IndexOf(const K &key) -> size_t {
+  std::scoped_lock<std::mutex> lock(latch_);
   int mask = (1 << global_depth_) - 1;
   return std::hash<K>()(key) & mask;
 }
@@ -66,17 +70,124 @@ auto ExtendibleHashTable<K, V>::GetNumBucketsInternal() const -> int {
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Find(const K &key, V &value) -> bool {
-  UNREACHABLE("not implemented");
+  // UNREACHABLE("not implemented");
+  size_t index = IndexOf(key);
+  if(index >= dir_.size()) {
+    return false;
+  }
+  
+  return dir_[index]->Find(key, value);
 }
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Remove(const K &key) -> bool {
-  UNREACHABLE("not implemented");
+  // UNREACHABLE("not implemented");
+  size_t index = IndexOf(key);
+  if(index >= dir_.size()) {
+    return false;
+  }
+  return dir_[index]->Remove(key);
+  
 }
 
 template <typename K, typename V>
 void ExtendibleHashTable<K, V>::Insert(const K &key, const V &value) {
-  UNREACHABLE("not implemented");
+  // UNREACHABLE("not implemented");
+  size_t index = IndexOf(key);
+  // printf("INSERT KEY HASH index: %ld\n", index);
+  if(index >= dir_.size()) {
+    return;
+  }
+    //遍历输出dir_
+  
+  // printf("==========\n");
+  // printf("dir_ size: %ld\n", dir_.size());
+  for(size_t i=0; i<dir_.size(); i++) {
+    // printf("dir_[%ld]: ", i);
+    // printf("local depth: %d size: %ld %p\n", dir_[i]->GetDepth(), dir_[i]->GetItems().size(), dir_[i].get());
+  }
+  // printf("==========\n");
+  if(!(dir_[index]->Insert(key, value))) {
+    // printf("Insert failed, cause full.\n");
+    // If is full
+
+    // If the local depth of the bucket is equal to the global depth
+    if(dir_[index]->GetDepth() == global_depth_) {
+      // printf("local depth == global depth\n");
+      // Increment the global depth and double the size of the directory
+      global_depth_ ++;
+      // printf("global depth: %d\n", global_depth_);
+      // Increment the local depth
+      dir_[index]->IncrementDepth();
+
+      size_t beforeDirSize = dir_.size();
+      // printf("beforeDirSize: %ld\n", beforeDirSize);
+      int beforeMask = (1 << (global_depth_-1)) -1;
+      int curMask = (1 << global_depth_) -1;
+      size_t newInsertIndex = IndexOf(key); //因为gd++了，所以要重新得一次。
+
+      for(int i=(int)beforeDirSize; i<(1 << global_depth_); i++){
+        if(((i&beforeMask) == (int)index) && ((i&curMask) != (int)index)) {
+          newInsertIndex = i;
+          std::shared_ptr<Bucket> before_b = dir_[index];
+          std::list<std::pair<K, V>> before_b_list = before_b->GetItems();
+          auto b = std::make_shared<Bucket>(bucket_size_, global_depth_);
+          // printf("new bucket: %p\n", b.get());
+          num_buckets_ ++;
+          //遍历这个桶
+          for(auto it=before_b_list.begin(); it!=before_b_list.end(); it++) {
+            //查询其中每一个元素的key
+            if(IndexOf(it->first) == newInsertIndex) {
+              // printf("桶替换: %ld\n", IndexOf(it->first));
+              b->Insert(it->first, it->second);
+              before_b->Remove(it->first);
+            }
+          }
+          dir_.push_back(b);
+        }else{
+          dir_.push_back(nullptr); 
+        }
+      }
+
+      dir_[IndexOf(key)]->Insert(key, value);
+      
+      for(int i=(int)beforeDirSize; i<(1 << global_depth_); i++){
+        if(dir_[i] == nullptr) {
+          dir_[i] = dir_[i & beforeMask];
+        }
+      }
+      
+
+    } else {
+      // printf("local depth < global depth\n");
+      // dir_不用double了. local_depth++就行
+      std::shared_ptr<Bucket> before_b = dir_[index];
+      std::list<std::pair<K, V>> before_b_list = before_b->GetItems();
+      int newlocalDepth = before_b->GetDepth() + 1;
+
+      //取出这个桶内第一个元素的新index作为样板,之后和这个相同的就保留,
+      //不相同的就放到新建的桶上.
+      int newlocalDepthMask = (1 << newlocalDepth) - 1;
+      size_t sampleIndex = std::hash<K>()(before_b_list.front().first) & newlocalDepthMask;
+      //新建一个桶
+      auto b = std::make_shared<Bucket>(bucket_size_, newlocalDepth);
+      num_buckets_ ++;
+      //遍历这个桶, 相同的保留,不相同的放到新建的桶上
+      for(auto it=++before_b_list.begin(); it!=before_b_list.end(); it++) {
+        if((std::hash<K>()(it->first) & newlocalDepthMask) != sampleIndex) {
+            b->Insert(it->first, it->second);
+            before_b->Remove(it->first);
+        }
+      }
+      //然后遍历dir_,重新指向
+      for(size_t i=0; i<dir_.size(); i++) {
+        if((dir_[i]==before_b) && ((i & newlocalDepthMask) != sampleIndex)) {
+          dir_[i] = b;
+        }
+      }
+      before_b->IncrementDepth();
+    }
+  }
 }
 
 //===--------------------------------------------------------------------===//
@@ -87,17 +198,41 @@ ExtendibleHashTable<K, V>::Bucket::Bucket(size_t array_size, int depth) : size_(
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Bucket::Find(const K &key, V &value) -> bool {
-  UNREACHABLE("not implemented");
+  for(auto it = list_.begin(); it != list_.end(); it++) {
+      // std::cout << it->first << " " << it->second << std::endl;
+      if(it->first == key) {
+        value = it->second;
+        return true;
+      }
+  }
+  return false;
 }
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Bucket::Remove(const K &key) -> bool {
-  UNREACHABLE("not implemented");
+  for(auto it = list_.begin(); it != list_.end(); it++) {
+    if(it->first == key) {
+      it = list_.erase(it);
+      return true;
+    }
+  }
+  return false;
 }
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Bucket::Insert(const K &key, const V &value) -> bool {
-  UNREACHABLE("not implemented");
+  if(IsFull()) {
+    return false;
+  }
+  for(auto it = list_.begin(); it != list_.end(); it++) {
+    if(it->first == key) {
+      // update
+      it->second = value;
+      return true;
+    }
+  }
+  list_.push_back({key, value});
+  return true;
 }
 
 template class ExtendibleHashTable<page_id_t, Page *>;
